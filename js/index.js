3ce11f376200072b8e119f6330442402b7bb6d09
Amari Ellington <amari.l.ellington@gmail.com>
2019-04-01 18:33:17 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 //you can use this to paste in js code and have it run when index.html is loaded
 
 let element = document.createElement('div');
-//you can use this to paste in js code and have it run when index.html is loaded	
+//you can use this to paste in js code and have it run when index.html is loaded
  element.innerHTML = 'Hello, DOM!';
 
  element.style.backgroundColor = '#f9f9f9';
